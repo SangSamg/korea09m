f3897675705f15fd5ea08693c59891c990a6113c
PSW <120436473+SangSamg@users.noreply.github.com>
2023-06-26 01:31:32 +0900

    Add files via upload

---
+++ js/mobile.js
@@ -0,0 +1,7 @@
+var UserAgent = navigator.userAgent;
+if(mobile){
+    window/location.href="https://sangsamg.github.io/korea09m/"
+}
+else{
+    alert{"http://www.korea09.co.kr/main/index.html"}
+}
