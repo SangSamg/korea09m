33a23a7a5cb38f430fe5c9cc40857ea18d6783b3
PSW <120436473+SangSamg@users.noreply.github.com>
2023-06-26 01:40:47 +0900

    Delete mobile.js

--- js/mobile.js
+++
@@ -1,7 +0,0 @@
-var UserAgent = navigator.userAgent;
-if(mobile){
-    window/location.href="https://sangsamg.github.io/korea09m/"
-}
-else{
-    alert{"http://www.korea09.co.kr/main/index.html"}
-}
